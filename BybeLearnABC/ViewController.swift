afa7e8e89e277bd2fb4393996497cd59e63395f1
windvally <windvally@sina.com>
2016-01-18 20:52:20 +0800

    Initial Commit

---
+++ BybeLearnABC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BybeLearnABC
+//
+//  Created by WangWei on 16/1/18.
+//  Copyright © 2016年 WangWei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
